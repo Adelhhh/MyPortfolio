2911005775806d6acdd41aff2add7ef9521e7812
AdelHassanpour <168416919+AdelHassanpour@users.noreply.github.com>
2024-09-17 11:57:09 +0200

    Initial Commit

---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Adel on 2024-09-17.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
